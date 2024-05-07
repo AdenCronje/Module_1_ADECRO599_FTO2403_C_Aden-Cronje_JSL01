0cdfd6a0976a17b9cc732c093e00b2501f8d4418
Aden Cronje <adencronje05@gmail.com>
2024-05-07 19:52:43 +0200

    Added new variables

--- script.js
+++ script.js
@@ -2,20 +2,22 @@ function validateSyntax() {
   let input = document.getElementById("petInput").value;
   // Validation logic goes here
   let result = "Valid"; // Placeholder for validation result
+  // let birthYear = Math.floor(Math.random);
+  let petName = "";
 
-  //   if (input == "pet_") {
-  //     result = "Valid Syntax";
-  //     console.log(result);
-  //   } else {
-  //     input === null, "pet";
-  //     result = "Invalid Syntax";
-  //     console.log(result);
-  //   }
+  if (input == "pet_") {
+    result = "Valid Syntax";
+    console.log(result);
+  } else {
+    input === null;
+    result = "Invalid Syntax";
+    // console.log(result);
+  }
 
   // TODO: Write your validation logic here
   // Check if input starts with 'pet_' and followed by alphanumeric characters
 
   document.getElementById("result").innerText = result;
 }
-let birthYear = Math.floor(Math.random() * 2000) + 100;
-console.log(birthYear);
+// let birthYear = Math.floor(Math.random() * 2000) + 100;
+// console.log(birthYear);
