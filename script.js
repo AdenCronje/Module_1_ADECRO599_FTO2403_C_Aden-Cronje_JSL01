72da0783d7f0304a62523c29098a6586baffbede
Aden Cronje <adencronje05@gmail.com>
2024-05-07 17:08:03 +0200

    Added if loop to function

--- script.js
+++ script.js
@@ -1,12 +1,21 @@
 function validateSyntax() {
-    let input = document.getElementById('petInput').value;
-    // Validation logic goes here
-    let result = ''; // Placeholder for validation result
+  let input = document.getElementById("petInput").value;
+  // Validation logic goes here
+  let result = "Valid"; // Placeholder for validation result
 
-    // TODO: Write your validation logic here
-        // Check if input starts with 'pet_' and followed by alphanumeric characters
+  //   if (input == "pet_") {
+  //     result = "Valid Syntax";
+  //     console.log(result);
+  //   } else {
+  //     input === null, "pet";
+  //     result = "Invalid Syntax";
+  //     console.log(result);
+  //   }
 
-            document.getElementById('result').innerText = result;
+  // TODO: Write your validation logic here
+  // Check if input starts with 'pet_' and followed by alphanumeric characters
+
+  document.getElementById("result").innerText = result;
 }
-
-
+let birthYear = Math.floor(Math.random() * 2000) + 100;
+console.log(birthYear);
